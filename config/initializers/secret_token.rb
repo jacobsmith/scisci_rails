0ecb095de9e4fe7db6c0b9aa8d20852a6abcd4e2
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-01-01 16:06:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScisciNotes::Application.config.secret_key_base = '45bfa12b5391cc03d73fd1389a72543533148188d62782827571d06359ae7d6c3ea4f0fb3135e6e3479986cf0c611ea55399598a3a93ec29e9fdd22591fe11a1'
