75b0b320f15a88d6fc4de0da518adecc72df108b
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-01-04 00:17:23 -0500

    added note route shortcuts to RoutesHelper

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,4 +1,9 @@
 module RoutesHelper
+
+##################
+## Source paths ##
+##################
+
   def edit_source_path(arg)
     if arg.is_a? Source 
       edit_project_source_path(arg.project, arg)
@@ -36,6 +41,50 @@ module RoutesHelper
       new_project_source_path(arg.project)
     elsif arg.is_a? Project
       new_project_source_path(arg)
+    end
+  end
+
+################
+## Note paths ##
+################
+
+  def notes_path(arg)
+    if arg.is_a? Note
+      project_source_notes_path(arg.source.project, arg.source, arg)
+    else
+      "There was an error"
+    end
+  end
+  
+  def note_path(arg)
+    if arg.is_a? Note
+      project_source_note_path(arg.source.project, arg.source, arg)
+    else
+      "There was an error"
+    end
+  end
+  
+  def edit_note_path(arg)
+    if arg.is_a? Note
+      edit_project_source_note_path(arg.source.project, arg.source, arg)
+    else
+      "There was an error"
+    end
+  end
+  
+  def destroy_note_path(arg)
+    if arg.is_a? Note
+      project_source_note_path(arg.source.project, arg.source, arg, method: :delete, data: { confirm: 'Are you sure?' }) 
+    else
+      "There was an error"
+    end
+  end
+
+  def new_note_path(arg)
+    if arg.is_a? Note
+      new_project_source_note_path(arg.source.project, arg.source)
+    else
+      arg.errors 
     end
   end
 end
