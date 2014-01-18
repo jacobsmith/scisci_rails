1a59e75d07fde53348a6b16ee37fce9da13fb620
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-01-18 01:03:25 -0500

    new manual routing scheme

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,30 @@
 ScisciNotes::Application.routes.draw do
-  resources :projects do
-    resources :sources do
-      resources :notes
-    end
-  end
 
+  match "users/:user_id", to: "projects#index", via: [:get]
+
+  match "users/:user_id/projects", to: "projects#index", via: :get
+  match "users/:user_id/projects", to: "projects#create", via: [ :post ]
+  match "users/:user_id/projects/new", to: "projects#new", via: [ :get ]
+  match "users/:user_id/projects/:project_id", to: "projects#show", via: :get
+  match "users/:user_id/projects/:project_id", to: "projects#update", via: [ :patch, :put ]
+  match "users/:user_id/projects/:project_id", to: "projects#destroy", via: [ :delete ]
+
+  match "users/:user_id/projects/:project_id/sources", to: "sources#index", via: :get  
+  match "users/:user_id/projects/:project_id/sources", to: "sources#create", via: :post 
+  match "users/:user_id/projects/:project_id/sources/new", to: "sources#new", via: :get 
+  match "users/:user_id/projects/:project_id/sources/:source_id", to: "sources#show", via: :get 
+  match "users/:user_id/projects/:project_id/sources/:source_id", to: "sources#update", via: [ :patch, :put ] 
+  match "users/:user_id/projects/:project_id/sources/:source_id", to: "sources#destroy", via: :delete 
+
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes", to: "notes#index", via: :get 
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes", to: "notes#create", via: :post
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes/new", to: "notes#new", via: :get
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes/:note_id", to: "notes#show", via: :get
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes/:note_id", to: "notes#update", via: [ :patch, :put ] 
+  match "users/:user_id/projects/:project_id/sources/:source_id/notes/:note_id", to: "notes#destroy", via: :delete
+  
   get 'projects/:project_id/tags/:name', to: 'tags#show', :as => :project_tags
-    
+      
   devise_for :users
   
   root :to => 'static_pages#home'
