8aa0e3306d5e4b7292dea034aff7316a1b691a6d
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-02-01 22:18:27 -0500

    add breadcrumbs helper to tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   include ApplicationHelper
+  include BreadcrumbHelper
+
   def show
     project_crumb
     @tags = Tag.all.where(project_id: params[:project_id], name: params[:name]) 
