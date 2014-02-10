e308a8c706c1a50969a6b2fe9b3ef355b6d6dc3a
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-02-09 23:57:57 -0500

    added citation creation to pop-up via modal

--- app/models/source.rb
+++ app/models/source.rb
@@ -11,4 +11,15 @@ class Source < ActiveRecord::Base
     @cite = Cite_Me.new
     @cite.generate_citation self
   end
+
+  def display_title
+    case self.source_type
+    when 'book'
+      self.title
+    when 'magazine'
+      self.title_of_article
+    when 'web'
+      self.name_of_site
+    end
+  end
 end
