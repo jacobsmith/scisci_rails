2d8d75f3507852d3c3e640681911635245b4126f
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-03-02 12:44:54 -0500

    bookEntry now has a link which will add the information to the source page (still need to add author support and a few other categories)

--- app/assets/javascripts/googleBooks.js
+++ app/assets/javascripts/googleBooks.js
@@ -1,4 +1,7 @@
 function googleBooks() {
+  // clear bookshelf if anything already exists
+  $('#bookshelf').empty();
+
   var title = $('#source_title').val();
   var maxResults = "10";
 
@@ -6,9 +9,64 @@ function googleBooks() {
 
   var result = $.getJSON(googleBooksUrl).done(function(response) {
     var books = result.responseJSON.items;
+    var bookshelf = $("#bookshelf");
+
+    // make books global to page so information can be accessed later
+    window.books = books;
+   
     for (var i = 0; i < books.length; i++) {
-      var bookshelf = $("#bookshelf");
-      bookshelf.append('<div class="book_entry">Title: ' + books[i].volumeInfo.title + '\n <img src="' + books[i].volumeInfo.imageLinks.smallThumbnail + '" /> </div>');
+      var bookInfo = books[i].volumeInfo;
+      var authors = "";
+      for (var j = 0; j < bookInfo.authors.length; j++) {
+        if ( bookInfo.authors[j] != undefined ) {
+          authors += bookInfo.authors[j];
+        };
+      };
+     
+      // append each result into #bookshelf
+      bookshelf.append('<div class="book_entry small-3 columns" id="book_entry#' + i + '">' + 
+        div( bookInfo.title, 'book_entry-title' ) +
+        div(authors, 'book_entry-authors') +
+        a("#", "Add Book Information", "addBook(" +  i + "); return false;", "book_entry-addBook") +
+        '<img src="' + books[i].volumeInfo.imageLinks.smallThumbnail + '" /> </div>');
     };
   });
 }
+
+function addBook(bookEntryId) {
+  var book = books[bookEntryId];
+  var bookInfo = book.volumeInfo;
+
+  $('#source_title').val( bookInfo.title );
+  $('#source_year_of_publication').val( bookInfo.publishedDate.slice(0,4) );
+  $('#source_publisher').val( bookInfo.publisher );
+}
+
+
+function div(arg, elementClass, id) {
+  var id = id || "";
+  var elementClass = elementClass || "";
+  var wrappedArg =  '<div class="' + elementClass + '" id="' + id + '">' + arg + '</div>';
+  return wrappedArg;
+}
+
+function a(target, text, onclick, elementClass) {
+  target = target || "#";
+  text = text || "Click Here";
+  onclick = onclick || "";
+
+  var wrappedA = '<a class="' + elementClass + '" + href="' + target +
+    '" onclick="' + onclick + '">' + text + '</a>';
+  return wrappedA;
+}
+/** Volume Info
+      authors (Array)
+      description (String)
+      imageLinks
+        smallThumbnail (url)
+        thumbnail (url)
+      title (String)
+      publisher (String)
+      publishedDate (yyyy-mm-dd)
+      canonicalVolumeLink (String)
+**/
