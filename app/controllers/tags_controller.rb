885bb7b023e19e5f13148057cacc63c2b273b4a9
Geoff Kimball <gakimball@bsu.edu>
2014-03-05 17:34:39 -0500

    Add initial styles for class header, show correct tag color on tag#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,7 +8,7 @@ class TagsController < ApplicationController
     project_crumb
     if current_user.can_read? Project.find(params[:project_id])
       @tags = Tag.all.where(project_id: params[:project_id], name: params[:name])
-      @tag_name = @tags.first.name
+      @tag = @tags.first
     end
   end
 
