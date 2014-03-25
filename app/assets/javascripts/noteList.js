0f2932319fe7da6a81c61acd7085f1513986ee2a
Geoff Kimball <gakimball@bsu.edu>
2014-03-25 02:26:41 -0400

    Add tag filtering to project notes view

--- app/assets/javascripts/noteList.js
+++ app/assets/javascripts/noteList.js
@@ -2,15 +2,43 @@
 
 var NoteList = function(elem) {
   this.$container = elem;
+  this.$notes     = this.$container.find('.note-list__item');
 
   this.initLayout();
+  if ($('[data-tagbrowser]').length > 0) {
+    this.initTagging();
+  }
 }
 NoteList.prototype = {
   initLayout: function() {
     this.$container.isotope({
       resizable: true
     });
-  }
+  },
+  initTagging: function() {
+    this.$notes.each(function() {
+      var tags = $(this).find('.tag a')
+        .map(function() {
+          return this.innerHTML;
+        })
+        .get()
+        .join(' ');
+      this.setAttribute('data-tags', tags);
+    });
+
+    this.events();
+  },
+  events: function() {
+    var self = this;
+
+    $(window).on('noteList.filter', function(event, data) {
+      var args = Array.prototype.slice.apply(arguments);
+      var tags = args.slice(1).join(' ');
+      self.$container.isotope({
+        filter: '[data-tags~="'+tags+'"]'
+      });
+    });
+  },
 }
 
 $(document).ready(function() {
---
+++ app/assets/javascripts/tagBrowser.js
@@ -0,0 +1,63 @@
+;(function() {
+
+var TagBrowser = function(elem) {
+  this.$container = elem;
+  if (elem.length === 0) return;
+
+  this.filters = [];
+  this.$tags = this.$container.find('.tag-list li');
+
+  this.init();
+  this.events();
+}
+TagBrowser.prototype = {
+  init: function() {
+    this.$tags.each(function() {
+      var tagName = $(this).children().eq(0).text();
+      this.setAttribute('data-tag', tagName);
+    });
+  },
+  events: function() {
+    var self = this;
+
+    this.$tags.click(function() {
+      if ($(this).hasClass('is-inactive') || self.filters.length === 0) {
+        self.addFilter(this);
+      }
+      else {
+        self.removeFilter(this);
+      }
+      return false;
+    });
+  },
+  addFilter: function(elem) {
+    var tagName = elem.getAttribute('data-tag');
+    this.filters.push(tagName);
+
+    if (this.filters.length === 1) {
+      this.$tags.addClass('is-inactive');
+    }
+    $(elem).removeClass('is-inactive');
+
+    this.$container.trigger('noteList.filter', this.filters);
+  },
+  removeFilter: function(elem) {
+    var tagName = elem.getAttribute('data-tag');
+    this.filters.splice(tagName, 1);
+
+    $(elem).addClass('is-inactive');
+    if (this.filters.length === 0) {
+      this.$tags.removeClass('is-inactive');
+    }
+    
+    this.$container.trigger('noteList.filter', this.filters);
+  }
+}
+
+$(document).ready(function() {
+  $('[data-tagbrowser]').each(function() {
+    var tagbrowser = new TagBrowser($(this));
+  })
+})
+
+})();
