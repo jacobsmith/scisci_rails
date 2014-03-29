2366b3f7cb2ed5ce3956ed47b3318bdb28405096
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-03-29 02:16:12 -0400

    updated deploy with nginx symlinks

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,15 @@ namespace :deploy do
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+  end
 end
 
 
