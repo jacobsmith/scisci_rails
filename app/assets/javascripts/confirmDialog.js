7024dc78d72b58c96e29f4eeff737aaa0afe66e6
Geoff Kimball <gakimball@bsu.edu>
2014-03-31 22:52:24 -0400

    Add confirm dialog JS, implement on tags#show

---
+++ app/assets/javascripts/confirmDialog.js
@@ -0,0 +1,46 @@
+;(function() {
+
+var ConfirmDialog = function(elem) {
+  this.$container = elem;
+  if (this.$container.length === 0) return;
+
+  this.message = this.$container.attr('data-confirm');
+  this.action  = this.$container.attr('href');
+
+  this.events();
+}
+
+ConfirmDialog.prototype = {
+  events: function() {
+    var self = this;
+    this.$container.click(function() {
+      self.create();
+      return false;
+    });
+  },
+
+  create: function() {
+    var html = [
+      '<div class="confirm-dialog">',
+        '<p class="confirm-dialog__message">'+this.message+'</p>',
+        '<a href="'+this.action+'" class="small warning button">Yes, delete</a>',
+        '<a data-close class="small secondary button">No, cancel</a>',
+      '</div>'
+    ].join('\n');
+
+    $(html)
+      .appendTo('body')
+      .find('[data-close]')
+        .click(function() {
+          $(this).closest('.confirm-dialog').remove();
+        });
+  }
+}
+
+$(document).ready(function() {
+  $('[data-confirm]').each(function() {
+    var confirmDialog = new ConfirmDialog($(this));
+  });
+});
+
+})();
