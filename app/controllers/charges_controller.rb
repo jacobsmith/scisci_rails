5f409d3d59f03d2c3637eb8f7c8be139f9f90a28
Jacob Smith <jacob.wesley.smith@gmail.com>
2014-05-04 03:15:23 -0400

    updated payment form and controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,16 +4,31 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 1000
+    @yearly_amount = 1000
+    @monthly_amount = 200
+    
+#    @params=params
+#    pry
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
-      :card => params[:stripeToken]
+      :card => params[:stripeToken],
+      :plan => params[:plan]
     )
+
+    ## Ensure plan and payment match up (to stop people like me from curl-ing weird data)
+    if params[:plan] == 'yearly_plan'
+      @amount = @yearly_amount
+    elsif params[:plan] == 'monthly_plan'
+      @amount = @monthly_amount
+    else
+      flash[:error] = "That is not a valid plan. Please try again."
+      redirect_to charges_path
+    end
 
     charge = Stripe::Charge.create(
       :customer   => customer.id,
-      :amount     => @amount,
+      :amount     => ( params[:plan] == 'yearly_plan' ? @yearly_amount : @monthly_amount ),
       :description => 'SciSci Notes Subscription',
       :currency    => 'usd'
     )
