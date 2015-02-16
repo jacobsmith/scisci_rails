cb11b24016a31bbaa6b3cc66a36cf6c326229556
Jacob Smith <jacob.wesley.smith@gmail.com>
2015-02-15 20:26:15 -0500

    allow notes to be seperated with semi-colons

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -21,4 +21,27 @@ describe Note do
     note_with_tags.tags.map(&:name).should include 'Tag2'
     note_with_tags.tags.map(&:name).should include 'Tag3'
   end
+
+  def tags_include(*tags)
+    tags.each do |tag|
+      note.tags.map(&:name).should include tag
+    end
+  end
+
+  describe "setting many tags" do
+    let(:note) { create(:note, quote: 'This is a test quote', comments: 'This is a test comment') }
+
+    it "accepts tags seperated by comma" do
+      note.tags = "tag1, tag2, tag3"
+      tags_include 'Tag1', 'Tag2', 'Tag3'
+      note.should be_valid
+    end
+    
+    it "accepts tags seperated by semi-colon" do
+      note.tags = "tag1; tag2; tag3"
+      tags_include 'Tag1', 'Tag2', 'Tag3'
+      note.should be_valid
+    end
+  end
+
 end
