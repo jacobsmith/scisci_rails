1b504da5ee89654907963f28c5a156e9e64af2a9
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-01-04 22:47:02 -0500

    make signup ajax from pricing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,13 @@ class StaticPagesController < ApplicationController
   end
 
   def pricing
+    # this is a bad hack because I want to let devise handle registration,
+    # so I just add a parameter to the URL after successful account creation
+    # and take them to the logged-in version of the sales page
+    if params[:sign_up_successful] == "true"
+      flash[:notice] = "Thanks for signing up, #{current_user.username}! You can now purchase a plan."
+      redirect_to view_plans_path
+    end
   end
 
   def about
